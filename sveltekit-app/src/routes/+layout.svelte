bd0289c155067e675f20a2c38b852b783e3622e6
Jakob9505 <wetzel375@gmail.com>
2025-09-23 10:15:43 +0200

    Layout:NavBar&Footer

--- sveltekit-app/src/routes/+layout.svelte
+++ sveltekit-app/src/routes/+layout.svelte
@@ -3,165 +3,105 @@
 	import { page } from '$app/stores';
 	import LiveMode from '../components/LiveMode.svelte';
 	import "../app.css";
-	interface Props {
-		children?: import('svelte').Snippet;
-	}
+	import logo from '$lib/assets/logo.webp';
+	const linksTop = [
+		{ href: '/', name: 'Home' },
+		{ href: '/trainingszeiten', name: 'Trainingszeiten' },
+		{ href: '/news', name: 'News' },
+		{ href: '/termine', name: 'Termine' },
+		{ href: '/ueber-uns', name: 'Über uns' },
+		{ href: '/kontakt', name: 'Kontakt' }
+	]
+	const linksBottom = [
+		{ href: '/datenschutz', name: 'Datenschutz' },
+		{ href: '/impressum', name: 'Impressum' },
+		{ href: '/kontakt', name: 'Kontakt' }
+	]
+	let showMenu = false;
 
-	let { children }: Props = $props();
+	function toggleMenu() {
+		showMenu = !showMenu;
+	}
 </script>
 
-{#if $isPreviewing}
-	<a href={`/preview/disable?redirect=${$page.url.pathname}`} class="preview-toggle">
-		<span>Preview Enabled</span>
-		<span>Disable Preview</span>
-	</a>
-{/if}
 
-<div class="container">
-	<header class="header">
-		<a class="header__title" href="/">SvelteKit + Sanity</a>
-	</header>
-	<main>
-		{@render children?.()}
-	</main>
-	<footer class="footer">
-		<p class="footer__text">
-			Made with <svg
-				data-sanity-icon="heart-filled"
-				width="1em"
-				height="1em"
-				viewBox="0 0 25 25"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				><path
-					d="M17 16C15.8 17.3235 12.5 20.5 12.5 20.5C12.5 20.5 9.2 17.3235 8 16C5.2 12.9118 4.5 11.7059 4.5 9.5C4.5 7.29412 6.1 5.5 8.5 5.5C10.5 5.5 11.7 6.82353 12.5 8.14706C13.3 6.82353 14.5 5.5 16.5 5.5C18.9 5.5 20.5 7.29412 20.5 9.5C20.5 11.7059 19.8 12.9118 17 16Z"
-					fill="currentColor"
-					stroke="currentColor"
-					stroke-width="1.2"
-				/></svg
-			> at Sanity
-		</p>
-	</footer>
-</div>
+<nav class="p-4">
+	<div class="container mx-auto flex justify-between items-center">
+		<img src={logo} alt="Logo Tan Gun Do" width="50" height="50"/>
+		<h1>Tan Gun Do Selbstverteidigung e.V</h1>
+	
+		<div class="hidden md:flex space-x-6">
+			{#each linksTop as linkT}
+				<a 
+				href={linkT.href} 
+				class:hover:text-gray-700={$page.url.pathname !== linkT.href}
+				class:hover:text-red-700={$page.url.pathname === linkT.href}
+				class:text-red-500={$page.url.pathname === linkT.href}
+				>
+					{linkT.name}
+				</a>
+			{/each}
+		</div>
+		<button class="md:hidden focus:outline-none" on:click={toggleMenu}>
+			&#9776;
+		</button>
+	</div>
+	{#if showMenu}
+		<div class="md:hidden mt-2 px-4 py-2 space-y-2">
+			{#each linksTop as linkT}
+				<a 
+				href={linkT.href} 
+				class="block py-2"
+				class:hover:text-gray-700={$page.url.pathname !== linkT.href}
+				class:hover:text-red-700={$page.url.pathname === linkT.href}
+				class:text-red-500={$page.url.pathname === linkT.href}
+				on:click={() => showMenu = false}
+				>
+					{linkT.name}
+				</a>
+			{/each}
+		</div>
+	{/if}
+</nav>
 
-{#if $isPreviewing}
-	<VisualEditing />
-	<LiveMode />
-{/if}
+<main>
+</main>
 
-<style>
-	.container {
-		margin: 0 auto;
+<hr class="border-t-1.5 border-gray-400 w-4/5 mx-auto my-5"/>
+<footer class="flex w-4/5 mx-auto my-5">
+	<nav class="p-4 ">
+		<div class="container mx-auto flex justify-center items-center">
+			<div class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6">
+				<img src={logo} alt="Logo Tan Gun Do" width="50" height="50"/>
+				<p>
+					<span class="block md:hidden">Tan Gun Do e.V.</span>
+					<span class="hidden md:block">&copy; 2025 Tan Gun Do SV e.V.</span>
+				</p>
+					{#each linksBottom as linkB}
+						<a
+						href={linkB.href}
+						class="font-thin text-gray-600 hover:underline"
+						>
+							{linkB.name}
+						</a>
+					{/each}
+			</div>
+
+		</div>
+	</nav>
+</footer>
+
+
+<style lang="postcss">
+	main{
+		display:flex;
+		flex-direction: column;
+		min-height: 100vh;
 	}
-
-	main {
-		margin-top: 45px;
-	}
-
-	.header {
-		display: flex;
-		padding: 0 var(--space-1);
-		border-bottom: 1px solid #ced2d9;
-
-		z-index: 10;
-		background: var(--white);
-		position: fixed;
-		left: 0;
-		right: 0;
-		top: 0;
-	}
-
-	.header .header__title {
-		font-weight: 800;
-		font-size: var(--font-size-3);
-		line-height: var(--line-height-1);
-		padding-left: var(--space-2);
-		margin: var(--space-3) 0;
-		text-decoration: none;
-		color: var(--black);
-	}
-
-	.footer {
-		display: flex;
-		justify-content: flex-end;
-		padding: 0 var(--space-3);
-	}
-
-	.footer .footer__text {
-		font-size: var(--font-size-1);
-		line-height: var(--line-height-1);
-		display: flex;
-		align-items: center;
-		gap: 2px;
-	}
-
-	@media (min-width: 575px) {
-		.container {
-			max-width: var(--max-width-1);
-			padding: 0 var(--space-4);
-		}
-
-		main {
-			margin-top: unset;
-		}
-
-		.header {
-			position: unset;
-			border-bottom: none;
-			margin: var(--space-3) 0;
-			padding: var(--space-2) 0;
-			background: unset;
-		}
-
-		.header .header__title {
-			margin: var(--space-3) 0 var(--space-2);
-			font-size: var(--font-size-5);
-		}
-
-		.footer {
-			margin: var(--space-3) 0;
-		}
-	}
-
-	.preview-toggle {
-		backdrop-filter: blur(12px);
-		border-radius: 0.25rem;
-		bottom: 1rem;
-		box-shadow:
-			0 10px 15px -3px rgba(0, 0, 0, 0.1),
-			0 4px 6px -2px rgba(0, 0, 0, 0.05);
-		color: #1f2937;
-		display: block;
-		font-size: 0.75rem;
-		font-weight: 500;
-		line-height: 1rem;
-		padding-bottom: 0.5rem;
-		padding-left: 0.75rem;
-		padding-right: 0.75rem;
-		padding-top: 0.5rem;
-		position: fixed;
-		right: 1rem;
+	footer{
+		flex: 0 0 absolute;
 		text-align: center;
-		text-decoration: none;
-		z-index: 50;
-	}
-
-	.preview-toggle:hover {
-		background-color: #ef4444;
-		color: #ffffff;
-	}
-
-	.preview-toggle span:first-child {
-		display: block;
-	}
-	.preview-toggle:hover span:first-child {
-		display: none;
-	}
-
-	.preview-toggle span:last-child {
-		display: none;
-	}
-	.preview-toggle:hover span:last-child {
-		display: block;
+		padding: 4px;
+		margin-top: auto;
 	}
 </style>
