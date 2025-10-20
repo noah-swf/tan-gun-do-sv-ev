d7bd3a9f672e5ce5a2a8df9c2036ceccec5e33b8
noah-swf <kingsleagueyt@gmail.com>
2025-10-20 22:08:20 +0200

    Add comment field to trainingszeiten schema and update related components

--- sveltekit-app/src/components/sections/TrainingszeitenSection.svelte
+++ sveltekit-app/src/components/sections/TrainingszeitenSection.svelte
@@ -4,14 +4,13 @@
     import {trainingszeitenQuery} from '$lib/sanity/queries';
     import Clock from '@lucide/svelte/icons/clock-4';
     import Pin from '@lucide/svelte/icons/map-pin';
-    import Person from '@lucide/svelte/icons/user';
-    import Flag from '@lucide/svelte/icons/corner-down-right';
+    import MessageSquare from '@lucide/svelte/icons/message-square';
 
     type Unit = {
         ageGroup: string;
         timeslot: string;
         location: string;
-        trainer: string;
+        comment?: string;
     };
 
     type Trainingszeiten = {
@@ -32,7 +31,7 @@
     });
 </script>
 
-<section class="px-6 md:px-10 py-10">
+<section class="px-6 md:px-10 mt-10 max-w-6xl mx-auto">
     <div class="hidden md:flex justify-between items-center mb-10">
       <h1 class="text-2xl font-semibold text-gray-800">Unsere Trainingszeiten</h1>
       <a href="/news" class="group inline-flex items-center gap-1 text-sm opacity-50 duration-200 text-black hover:opacity-100">
@@ -46,8 +45,8 @@
     </div>
 
     <div class="relative flex md:items-center text-left md:text-center bg-yellow-100 border-2 border-yellow-200 rounded-lg p-4 mb-10">
-        <Clock color="var(--color-yellow-600)" class="w-5 h-5 text-yellow-600 absolute left-4" />
-        <span class="text-yellow-600 text-center w-full">
+        <Clock color="var(--color-yellow-600)" class="w-5 h-5 text-yellow-600 left-4" />
+        <span class="text-yellow-600 text-left md:text-center w-full ml-3">
             <strong>Hinweis:</strong> Sollten Trainingszeiten abweichen, informieren wir euch immer rechtzeitig über WhatsApp.
         </span>
     </div>
@@ -62,28 +61,28 @@
 
     {#each trainingszeiten as day}
         <section class="mb-10">
-            <h2 class="text-2xl font-semibold mb-6 capitalize">{day.day}</h2>
+            <h2 class="text-xl font-semibold mb-6 capitalize">{day.day}</h2>
             
 
             <div class="flex flex-wrap gap-8 justify-start">
 
                 {#each day.units as unit}
-                    <div class="border border-gray-300 rounded-xl p-4 w-100 space-y-1">
-                        <p class="font-bold text-lg">{unit.ageGroup}</p>
+                    <div class="border border-gray-300 rounded-xl p-4 w-100 space-y-2">
+                        <p class="font-bold text-base">{unit.ageGroup}</p>
                         <hr class="border-t-1.5 border-gray-300 mb-4 mt-2">
                         <div class="flex items-center">
-                            <Clock color="var(--color-red)" class="w-5 h-5 mr-2"/>
-                            <p class="text-gray-700">{unit.timeslot}</p>
+                            <Clock color="var(--color-red)" class="w-4 h-4 mr-2"/>
+                            <p class="text-gray-700 text-sm">{unit.timeslot}</p>
                         </div>
                         <div class="flex items-center">
-                            <Pin color="var(--color-red)" class="w-5 h-5 mr-2"/>
-                            <p class="text-gray-700">{unit.location}</p>
+                            <Pin color="var(--color-red)" class="w-4 h-4 mr-2"/>
+                            <p class="text-gray-700 text-sm">{unit.location}</p>
                         </div>
-                        {#if day.day === 'Samstag'}
-                        <div class="flex items-center">
-                        <Flag color="var(--color-red)" class="w-5 h-5 mr-2 mt-2"/>
-                        <p class="text-sm text-gray-500 mt-2"><strong>Kampftraining:</strong> Schutzausrüstung mitbringen</p>
-                        </div>
+                        {#if unit.comment }
+                            <div class="flex items-center">
+                                <MessageSquare  color="var(--color-red)" class="w-4 h-4 mr-2"/>
+                                <p class="text-sm text-gray-700 ">"{unit.comment}"</p>
+                            </div>
                         {/if}
                     </div>
                 {/each}
--- sveltekit-app/src/lib/sanity/queries.ts
+++ sveltekit-app/src/lib/sanity/queries.ts
@@ -33,7 +33,7 @@ export const trainingszeitenQuery = groq`
 	    ageGroup,
 	    timeslot,
 	    location,
-	    trainer
+	    comment
 	}
   }`;
 
